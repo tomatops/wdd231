13c66c81ee8cf29c8fcdd38aeaf7d3ac5dad640d
tomatops <mthea.tirazona@gmail.com>
2025-11-08 22:10:10 +0800

    Week 2 Chamber Site

--- chamber/scripts/directory.js
+++ chamber/scripts/directory.js
@@ -1,8 +1,21 @@
 const url = 'data/members.json';
 const gridButton = document.querySelector("#grid");
 const listButton = document.querySelector("#list");
+const display = document.querySelector("article");
 const cards = document.querySelector('#cards');
 
+// CARD MENU
+gridButton.addEventListener("click", () => {
+    display.classList.add("grid");
+    display.classList.remove("list");
+});
+
+listButton.addEventListener("click", () => {
+    display.classList.add("list");
+    display.classList.remove("grid");
+});
+
+// COMPANY CARDS
 const displayCompanies = (companies) => {
     companies.forEach((company) => {
         let card = document.createElement('section');
@@ -10,18 +23,20 @@ const displayCompanies = (companies) => {
         let logo = document.createElement('img');
         let address = document.createElement('p');
         let phone = document.createElement('p');
-        let website = document.createElement('p');
+        let website = document.createElement('a');
 
         name.textContent = company.name;
         address.textContent = company.address;
         phone.textContent = company.phone_number;
+
         website.textContent = company.website_url;
+        website.setAttribute('href', company.website_url);
 
         logo.setAttribute('src', company.image_url);
         logo.setAttribute('alt', `${company.name} logo`);
         logo.setAttribute('loading', 'lazy');
-        logo.setAttribute('width', '350');
-        logo.setAttribute('height', '350');
+        logo.setAttribute('width', '100');
+        logo.setAttribute('height', '100');
 
         card.appendChild(name);
         card.appendChild(logo);
