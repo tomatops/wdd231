59f5330abbe63e4018deaa232b3ab5624556e945
tomatops <mthea.tirazona@gmail.com>
2025-11-29 01:12:59 +0800

    Week 3 Chamber Site

--- chamber/scripts/home.js
+++ chamber/scripts/home.js
@@ -14,13 +14,13 @@ const sunset = document.querySelector('#sunset');
 
 const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&units=metric&appid=${key}`;
 
-async function apiFetch() {
+async function getWeather() {
     try {
         const response = await fetch(url);
         if (response.ok) {
             const data = await response.json();
             console.log(data);
-            displayResults(data);
+            displayWeather(data);
         } else {
             throw Error(await response.text());
         }
@@ -28,9 +28,9 @@ async function apiFetch() {
         console.log(error);
     }
 }
-apiFetch();
+getWeather();
 
-function displayResults(data) {
+function displayWeather(data) {
     let desc = data.weather[0].description;
 
     const iconsrc = `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
@@ -66,13 +66,13 @@ const weatherDay3 = document.querySelector('#day3');
 
 const url2 = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${long}&units=metric&appid=${key}`;
 
-async function apiFetch2() {
+async function getForecast() {
     try {
         const response2 = await fetch(url2);
         if (response2.ok) {
             const data2 = await response2.json();
             console.log(data2);
-            displayResults2(data2);
+            displayForecast(data2);
         } else {
             throw Error(await response2.text());
         }
@@ -80,9 +80,9 @@ async function apiFetch2() {
         console.log(error);
     }
 }
-apiFetch2();
+getForecast();
 
-function displayResults2(data) {
+function displayForecast(data) {
     const convertDay = (timestamp) => {
         return new Date(timestamp * 1000).toLocaleDateString('en-US', { weekday: 'long' });
     };
@@ -91,3 +91,61 @@ function displayResults2(data) {
     weatherDay2.innerHTML = `${convertDay(data.list[2].dt)}: <strong>${data.list[2].main.temp}&deg; C</strong>`;
     weatherDay3.innerHTML = `${convertDay(data.list[10].dt)}: <strong>${data.list[10].main.temp}&deg; C</strong>`;
 }
+
+// Business Cards
+const url3 = 'data/members.json';
+const businessCards = document.querySelector('#business-cards');
+
+const displayCompanies = (companies) => {
+    const filterCompanies = companies.filter(company => company.membership === 2 || company.membership === 3).slice(0, 3);
+
+    filterCompanies.forEach((company) => {
+        let card = document.createElement('section');
+        let name = document.createElement('h3');
+        let cardGrid = document.createElement('div');
+        let cardList = document.createElement('ul');
+        let logo = document.createElement('img');
+        let address = document.createElement('li');
+        let phone = document.createElement('li');
+        let websiteList = document.createElement('li');
+        let membership = document.createElement('li');
+        let website = document.createElement('a');
+
+        name.textContent = company.name;
+        address.innerHTML = `<strong>Address:</strong> ${company.address}`;
+        phone.innerHTML = `<strong>Phone:</strong>  ${company.phone_number}`;
+        membership.innerHTML = `<strong>Membership:</strong>  ${company.membership}`;
+
+        websiteList.innerHTML = `<strong>URL:</strong> `;
+        website.innerHTML = company.website_url;
+        website.setAttribute('href', company.website_url);
+        website.setAttribute('target', '_blank');
+
+        logo.setAttribute('src', company.image_url);
+        logo.setAttribute('alt', `${company.name} logo`);
+        logo.setAttribute('loading', 'lazy');
+        logo.setAttribute('width', '100');
+        logo.setAttribute('height', '100');
+
+        cardGrid.setAttribute('class', 'card-grid');
+
+        card.appendChild(name);
+        websiteList.appendChild(website);
+        cardList.appendChild(address);
+        cardList.appendChild(phone);
+        cardList.appendChild(websiteList);
+        cardList.appendChild(membership);
+        cardGrid.appendChild(logo);
+        cardGrid.appendChild(cardList);
+        card.appendChild(cardGrid);
+        businessCards.appendChild(card);
+    });
+}
+
+async function getCompanyData() {
+    const response3 = await fetch(url3);
+    const data3 = await response3.json();
+    displayCompanies(data3.companies);
+}
+
+getCompanyData();
