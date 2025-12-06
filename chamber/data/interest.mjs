bc0007ad4ef3a74a2d1c5840c083bd792b7fd1dd
tomatops <mthea.tirazona@gmail.com>
2025-12-06 23:59:32 +0800

    Week 5 Chamber Site

--- chamber/data/interest.mjs
+++ chamber/data/interest.mjs
@@ -3,49 +3,49 @@ export const areaInterest = {
         {
             name: "Hidden Valley Springs",
             address: "Calauan, Laguna",
-            image: "https://tomatops.github.io/wdd231/chamber/images/interest/hidden-valley.webp",
+            image: "images/interest/hidden-valley.webp",
             description: "A secluded natural resort offering a refreshing change of pace with natural pools, forest trekking, and cool springs."
         },
         {
             name: "Isdaan Floating Restaurant",
             address: "Bay, Laguna",
-            image: "https://tomatops.github.io/wdd231/chamber/images/interest/isdaan.webp",
+            image: "images/interest/isdaan.webp",
             description: "The restaurant features floating bamboo huts on a man-made pond and offers a diverse menu along with seafood dishes. Guests can also enjoy live music, boat rides, and various cultural performances."
         },
         {
             name: "IsKarGu Restaurant",
             address: "Calauan, Laguna",
-            image: "https://tomatops.github.io/wdd231/chamber/images/interest/iskargu.webp",
+            image: "images/interest/iskargu.webp",
             description: "The restaurant offers a unique dining experience with its affordable prices, delicious food, and magnificent views of rice fields and mountains. It is recommended for those looking to enjoy a traditional Filipino dining experience with a scenic backdrop."
         },
         {
             name: "Itik Park",
             address: "Victoria, Laguna",
-            image: "https://tomatops.github.io/wdd231/chamber/images/interest/itik-park.webp",
+            image: "images/interest/itik-park.webp",
             description: "The park showcases the town's rich cultural heritage and its affinity for duck farming features impressive giant representations of waterfowl, symbolizing the local community's livelihood."
         },
         {
             name: "Mount Kalisungan",
             address: "Calauan, Laguna",
-            image: "https://tomatops.github.io/wdd231/chamber/images/interest/mount-kalisungan.webp",
+            image: "images/interest/mount-kalisungan.webp",
             description: "It is known for its accessible hiking trails and picturesque landscapes. The mountain stands at approximately 760 meters above sea level and offers a moderate challenge for hikers, making it suitable for both beginners and experienced trekkers."
         },
         {
             name: "Calauan Plaza",
             address: "Calauan, Laguna",
-            image: "https://tomatops.github.io/wdd231/chamber/images/interest/plaza.webp",
+            image: "images/interest/plaza.webp",
             description: "A vibrant public space located in the heart of Calauan. It serves as a central gathering place for the community, featuring open spaces, greenery, and various shops and eateries."
         },
         {
             name: "Sampaloc Lake",
             address: "San Pablo City, Laguna",
-            image: "https://tomatops.github.io/wdd231/chamber/images/interest/sampaloc-lake.webp",
+            image: "images/interest/sampaloc-lake.webp",
             description: "The largest of the Seven Lakes of San Pablo and is known for its calm waters, creating a peaceful escape for relaxation and recreational activities. It is home to a variety of freshwater fish, including mudfish, tilapia, catfish, and more."
         },
         {
             name: "Yambo Lake",
             address: "Nagcarlan, Laguna",
-            image: "https://tomatops.github.io/wdd231/chamber/images/interest/yambo-lake.webp",
+            image: "images/interest/yambo-lake.webp",
             description: "A maar lake formed by a volcanic eruption, classified as part of the larger Laguna Volcanic Field. It is a favored destination for activities such as bamboo rafting and swimming, offering a tranquil setting surrounded by lush vegetation."
         },
     ]
